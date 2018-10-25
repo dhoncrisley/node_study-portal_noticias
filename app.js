75a02b7345d0dc2848791ad97a88e9783e64a335
Saulo Wilson <saulowilson@outlook.com>
2018-10-25 17:28:25 -0300

    express get

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send("<html><body>Portal de notícias</body></html>");
+})
+app.get('/tecnology', function(req, res){
+    res.send("<html><body>Retornou tecnology</body></html>");
+})
+
+app.listen(3000, function(){
+    console.log('subiu');
+})
