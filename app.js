6a604f74841292647a760b26409090ee502fabee
Saulo Wilson <saulowilson@outlook.com>
2018-10-29 13:40:01 -0300

    refactoring mysql connection

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var app = require('./config/server');
-var rotaNoticias = require('./app/routes/noticias');
-var rotaHome = require('./app/routes/home');
-var rotaForm = require('./app/routes/formulario_inclusao_noticia');
-rotaNoticias(app);
-rotaHome(app);
-rotaForm(app);
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaHome = require('./app/routes/home')(app);
+var rotaForm = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function(){
     console.log('||Servidor iniciado||');
