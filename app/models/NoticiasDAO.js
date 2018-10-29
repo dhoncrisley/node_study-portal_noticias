8100c41fecd0d2c2d477102407619a733bf6450f
Saulo Wilson <saulowilson@outlook.com>
2018-10-29 17:26:30 -0300

    views data binding

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -3,7 +3,7 @@ function NoticiasDAO(connection) {
 
 }
 NoticiasDAO.prototype.getNoticias = this.getNoticias = function (callback) {
-    this._connection.query('select * from noticias', callback);
+    this._connection.query('select * from noticias order by data desc', callback);
 }
 NoticiasDAO.prototype.getNoticia = this.getNoticia = function (callback) {
     this._connection.query('select * from noticias where id = 2', callback);
@@ -11,6 +11,10 @@ NoticiasDAO.prototype.getNoticia = this.getNoticia = function (callback) {
 NoticiasDAO.prototype.salvarNoticia = this.salvarNoticia = function (noticia, callback) {
     
     this._connection.query('insert into noticias set ?', noticia, callback);
+}
+NoticiasDAO.prototype.get5UltimasNoticias = this.get5UltimasNoticias = function ( callback) {
+    this._connection.query('select * from noticias order by data desc limit 5', callback);
+ 
 }
 
 
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
     app.get('/formulario_inclusao_noticia', function (req, res) {
        app.app.controllers.admin.formulario_inclusao_noticia(app, req, res)
     })
-    app.post('/noticia/salvar', function (req, res) {
+    app.post('/noticias/salvar', function (req, res) {
         app.app.controllers.admin.noticias_salvar(app, req, res)
         
     })
