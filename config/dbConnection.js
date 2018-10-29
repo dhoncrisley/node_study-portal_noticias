06cf00a58be41f4c99cdd4dfab2ef32688a0ed5d
Saulo Wilson <saulowilson@outlook.com>
2018-10-29 14:33:40 -0300

    creating models for the application

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,13 @@
 var mysql = require('mysql');
-
-module.exports = function(){
-
+var connMySQL = function(){
+    console.log('conexao com o db estabelecida')
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'portal_noticias'
     });
+}
+module.exports = function(){
+    return connMySQL;
 }
